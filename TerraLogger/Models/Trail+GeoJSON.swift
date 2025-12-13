021c3c488554fc397f3a85c2bf54282a90f9727c
Seb Martin <seb.d.martin@gmail.com>
2025-12-12 20:06:45 -0500

    Improve GeoJSON import to support featureCollection (still crude)

--- TerraLogger/Models/Trail+GeoJSON.swift
+++ TerraLogger/Models/Trail+GeoJSON.swift
@@ -4,15 +4,38 @@
 //
 //  Created by Seb Martin on 2025-11-23.
 //
-
+import os
 import Turf
+
+fileprivate let logger = Logger.importer
 
 extension Trail {   
     convenience init?(fromGeoJSON geojson: GeoJSONObject, name: String? = nil) {
-        guard case let .feature(feature) = geojson,
-              case let .lineString(lineString) = feature.geometry else {
+        switch geojson {
+        case .feature(let feature):
+            if case let .lineString(lineString) = feature.geometry {
+                self.init(lineString, name: name)
+                return
+            }
+            return nil
+            
+        case .featureCollection(let collection):
+            for feature in collection.features {
+                if case let .lineString(lineString) = feature.geometry {
+                    self.init(lineString, name: name)
+                    return
+                }
+            }
+            logger.error("Could not pares GeoJSON: \(String(describing: collection))")
+            return nil
+
+        default:
+            logger.error("Could not pares GeoJSON: \(String(describing: geojson))")
             return nil
         }
+    }
+    
+    private convenience init?(_ lineString: LineString, name: String? = nil) {
         let coordinates = lineString.coordinates.map {
             Coordinate(from: $0)
         }
